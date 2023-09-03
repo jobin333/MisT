6ceef49c7375ca4b314305f389250f114812fdac
jobin333 <jobin.jbn@gmail.com>
2023-09-03 11:27:29 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+  
+setup(
+    name='mvit',
+    version='0.1',
+    description='Multiscale vision transformer for Cholec80 dataset classification',
+    author='Jobin Jose',
+    author_email='jobin.jbn@gmail.com',
+    packages=find_packages(),
+    install_requires=[ ],
+)
