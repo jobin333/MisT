ad78baa9a235a9b9f3162638eb057df02095d10f
jobin333 <jobin.jbn@gmail.com>
2023-12-10 16:41:52 +0530

    minor

--- mvit/models/transformer_models.py
+++ mvit/models/transformer_models.py
@@ -7,7 +7,8 @@ from data_utils import PositionalEncoding
 class TransformerModel(nn.Module):
 
     def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
-                 nlayers: int, device, dropout: float = 0.5):
+                 nlayers: int, device, dropout: float = 0.5,
+                 enable_positional_encoding=True, enable_src_mask=True):
         super().__init__()
         self.model_type = 'Transformer'
         self.pos_encoder = PositionalEncoding(d_model, dropout)
@@ -17,6 +18,8 @@ class TransformerModel(nn.Module):
         self.d_model = d_model
         self.linear = nn.Linear(d_model, ntoken)
         self.device = device
+        self.enable_positional_encoding = enable_positional_encoding
+        self.enable_src_mask = enable_src_mask
         self.init_weights()
 
     def init_weights(self) -> None:
@@ -35,12 +38,14 @@ class TransformerModel(nn.Module):
             output Tensor of shape ``[seq_len, batch_size, ntoken]``
         """
         src = self.embedding(src) * math.sqrt(self.d_model)
-        src = self.pos_encoder(src)
-        if src_mask is None:
+        if self.enable_positional_encoding:
+            src = self.pos_encoder(src)
+        if src_mask is None and self.enable_src_mask:
             """Generate a square causal mask for the sequence. The masked positions are filled with float('-inf').
             Unmasked positions are filled with float(0.0).
             """
             src_mask = nn.Transformer.generate_square_subsequent_mask(len(src)).to(self.device)
-        output = self.transformer_encoder(src, src_mask)
+        if self.enable_src_mask:
+            output = self.transformer_encoder(src, src_mask)
         output = self.linear(output)
         return output
