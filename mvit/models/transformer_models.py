18c966652cef1c177039fa74ed1c705fc6546aa9
jobin333 <jobin.jbn@gmail.com>
2023-12-15 16:06:38 +0530

    minor

--- mvit/models/transformer_models.py
+++ mvit/models/transformer_models.py
@@ -15,19 +15,11 @@ class TransformerModel(nn.Module):
             self.pos_encoder = PositionalEncoding(d_model, dropout)
         encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.embedding = nn.Embedding(ntoken, d_model)
         self.d_model = d_model
         self.linear = nn.Linear(d_model, ntoken)
         self.device = device
         self.enable_positional_encoding = enable_positional_encoding
         self.enable_src_mask = enable_src_mask
-        self.init_weights()
-
-    def init_weights(self) -> None:
-        initrange = 0.1
-        self.embedding.weight.data.uniform_(-initrange, initrange)
-        self.linear.bias.data.zero_()
-        self.linear.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, src: Tensor, src_mask: Tensor = None) -> Tensor:
         """
@@ -38,7 +30,6 @@ class TransformerModel(nn.Module):
         Returns:
             output Tensor of shape ``[seq_len, batch_size, ntoken]``
         """
-        src = self.embedding(src) * math.sqrt(self.d_model)
         if self.enable_positional_encoding:
             src = self.pos_encoder(src)
         if src_mask is None and self.enable_src_mask:
