f7202e7e1cdd32b2fcbf299f94557ce0975afd71
jobin333 <jobin.jbn@gmail.com>
2024-02-04 23:02:09 +0530

    minor

--- mvit/models/bayesian.py
+++ mvit/models/bayesian.py
@@ -1,6 +1,6 @@
 import torch
 
-def get_trans_matrix():
+def get_trans_matrix(state_stay_factor, state_seq_factor, regularization_factor):
     other_trans_matrix  = [
         [0,0,1,0,0,0,0],
         [0,0,0,0,1,1,0],
@@ -13,16 +13,15 @@ def get_trans_matrix():
     other_trans_matrix = torch.tensor(other_trans_matrix)
     self_trans_matrix = torch.eye(7) 
 
-#     trans_matrix = other_trans_matrix*.1 + self_trans_matrix*.3 + torch.ones((7,7))
-    trans_matrix = other_trans_matrix + self_trans_matrix
+    trans_matrix = other_trans_matrix*state_seq_factor + self_trans_matrix*state_stay_factor + torch.ones((7,7))*regularization_factor
     return trans_matrix
 
 def get_bayesian_prob(last_prediction, prob, trans_matrix):
     bayesian_prob = trans_matrix[last_prediction] * prob
     return bayesian_prob.argmax(-1)
 
-def generate_bayesian_prediction(x):
-    trans_matrix = get_trans_matrix()
+def generate_bayesian_prediction(x, state_stay_factor=0.3, state_seq_factor=0.1, regularization_factor=1.):
+    trans_matrix = get_trans_matrix(state_stay_factor, state_seq_factor, regularization_factor)
     trans_matrix = trans_matrix.to(x.device)
     last_prediction = x[0].argmax(-1)
     predictions = []
