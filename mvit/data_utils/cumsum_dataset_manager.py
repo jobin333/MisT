bab1e9f4e3d4f68cca1fa906cdc4c046afe17bbf
jobin333 <jobin.jbn@gmail.com>
2024-03-19 18:55:08 +0530

    minor

--- mvit/data_utils/cumsum_dataset_manager.py
+++ mvit/data_utils/cumsum_dataset_manager.py
@@ -2,22 +2,40 @@ import torch
 import os 
 from torch.utils.data import DataLoader
 from mvit.logging_utils.logger import logger
-import numpy as np
 
 module_logger = logger.getChild(__name__)
+class DictDataset():
+  def __init__(self, data, feature_keys, label_key):
+    self.data = data
+    self.feature_keys = feature_keys
+    self.label_key = label_key
+
+  def __getitem__(self, idx):
+    features = [self.data[key][idx] for key in self.feature_keys]
+    features = torch.cat(features)
+    label = self.data[self.label_key][idx]
+    return features, label
+
+  def __len__(self):
+    return len(self.data[self.label_key])
+
 
 class DatasetManager():
-  def __init__(self, feature_path, discount=0.9):
+  def __init__(self, feature_path, discount=0.9, batch_size=32, shuffle=False):
     indices = range(1, 81)
     self.feature_path = feature_path
+    self.batch_size = batch_size
+    self.shuffle = shuffle
     self.discount = discount
     self.data = self.load_files(indices)
 
   def __len__(self):
     return len(self.data)
 
-  def get_dataloader(self, idx):
-      return self.data[idx+1]
+  def get_dataloader(self, idx, feature_keys, label_key):
+      ds =  self.data[idx]
+      ds =  DictDataset(ds, feature_keys, label_key)
+      return DataLoader(ds, batch_size=self.batch_size, shuffle=self.shuffle)
 
   def load_files(self, indices):
     file_paths = [os.path.join(self.feature_path, f'tensors_{i}.pt') for i in indices]
