01cd713a662a2bdf572fba925dc9d658712fd59d
jobin333 <jobin.jbn@gmail.com>
2024-06-08 20:11:20 +0530

    updated logger

--- mvit/logging_utils/logger.py
+++ mvit/logging_utils/logger.py
@@ -2,19 +2,27 @@ import logging
 
 ## Configurations
 logger_configs_ = {
-    'logging_level': logging.DEBUG,
+    'file_logging_level': logging.DEBUG,
+    'console_logging_level': logging.DEBUG,
     'logging_file': 'mvit_debugging.log'
 }
 
 def get_logger_():
 
-    file_handler = logging.FileHandler(logger_configs_['logging_file'], 'w')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    file_handler = logging.FileHandler(logger_configs_['logging_file'], 'w')
     file_handler.setFormatter(formatter)
-    file_handler.setLevel(logger_configs_['logging_level'])
+    file_handler.setLevel(logger_configs_['file_logging_level'])
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logger_configs_['console_logging_level'])
+
     logger = logging.getLogger('MVIT')
     logger.addHandler(file_handler)
-    logger.setLevel(logger_configs_['logging_level'])
+    logger.setLevel(logger_configs_['file_logging_level'])
+    logger.addHandler(console_handler)
     return logger
 
 
