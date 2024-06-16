0a4e3a6a94d9019f780d8cf74043a58bd7eaa580
jobin333 <jobin.jbn@gmail.com>
2024-06-16 18:34:57 +0530

    error correction of TrainerConfigurationGenerator

--- mvit/train_utils/config_generator.py
+++ mvit/train_utils/config_generator.py
@@ -9,17 +9,21 @@ from mvit.data_utils.global_dataset_manager import ModelOuptutDatasetManager
 from mvit.metrics.metrics import Accuracy
 
 
-
 class TrainerConfigurationGenerator():
     
     def __init__(self, *args):
         if len(args) == 1: 
-            self.__dict__['_config'] = self.load(args[0])
+            file_path = args[0]
+            self.__dict__['_config'] = torch.load(file_path)
         else:
-            self.__dict__['_config']  = self.generate_config(*args)
+            dataset_name, feature_model_name, basic_config = args
+            self.__dict__['_config'] = basic_config
+            special_config = self.generate_special_config(dataset_name, feature_model_name)
+            for key, value in special_config.items():
+                self.__dict__['_config'][key] = value      
             
             
-    def get_file_indices(self, dataset_name, randomize):
+    def get_file_indices(self, dataset_name):
         dataset_spec = self.dataset_details[dataset_name]
         video_count = dataset_spec['video_count']
         contain_test_set = False if type(video_count)==int else True
@@ -30,7 +34,7 @@ class TrainerConfigurationGenerator():
 
         else:
             file_indices = list( range(1, video_count+1) )
-            if randomize:
+            if self.random_train_test:
                 random.shuffle(file_indices)
 
             test_file_count = dataset_spec['test_file_count']
@@ -40,18 +44,18 @@ class TrainerConfigurationGenerator():
         return train_file_indices, test_file_indices, contain_test_set
     
 
-    def get_file_paths(self, dataset_name, feature_model_name, model_save_folder, first_level_model_name='fl', 
+    def get_file_paths(self, dataset_name, feature_model_name, first_level_model_name='fl', 
                                    second_level_model_name='sl', max_files=100):
 
         for idx in range(max_files):
             flm_save_file_name = f'{dataset_name}_{feature_model_name}_{first_level_model_name}_{idx}.pt'
-            flm_save_param_path = os.path.join(model_save_folder, flm_save_file_name)
+            flm_save_param_path = os.path.join(self.model_save_folder, flm_save_file_name)
 
             slm_save_file_name = f'{dataset_name}_{feature_model_name}_{second_level_model_name}_{idx}.pt'
-            slm_save_param_path = os.path.join(model_save_folder, slm_save_file_name)
+            slm_save_param_path = os.path.join(self.model_save_folder, slm_save_file_name)
 
             config_file_name = f'{dataset_name}_{feature_model_name}_{idx}_pt.config'
-            config_file_path = os.path.join(model_save_folder, config_file_name)
+            config_file_path = os.path.join(self.model_save_folder, config_file_name)
 
             if not os.path.exists(config_file_path):
                 break
@@ -60,19 +64,12 @@ class TrainerConfigurationGenerator():
         
     
 
-    def generate_config(self, dataset_name, feature_model_name, global_config):
-        model_save_folder = global_config ['model_save_folder']
-        random_train_test = global_config['random_train_test']
+    def generate_special_config(self, dataset_name, feature_model_name):
         in_features = self.feature_model_details[feature_model_name]
         out_features = self.dataset_details[dataset_name]['num_classes']
         
-        flm_save_param_path, slm_save_param_path, config_file_path =  self.get_file_paths(dataset_name, feature_model_name,
-                                                                                    model_save_folder)
-
-        train_file_indices, test_file_indices, contain_test_set = self.get_file_indices(dataset_name, random_train_test)
-
-
-        config = global_config.copy()
+        flm_save_param_path, slm_save_param_path, config_file_path =  self.get_file_paths(dataset_name, feature_model_name)
+        train_file_indices, test_file_indices, contain_test_set = self.get_file_indices(dataset_name)
 
         special_config = {
             'train_file_indices': train_file_indices,
@@ -88,20 +85,15 @@ class TrainerConfigurationGenerator():
 
         }
         
-        for key, value in special_config.items():
-            config[key] = value
-            
-        return config
+        return special_config
     
     
     def __getattr__(self, name):
-        return self._config[name]
+        value = self.__dict__['_config'][name]
+        return value
     
     def __setattr__(self, name, value):            
-        self._config[name] = value
-    
-    def load(self, filename):
-        return torch.load(filename)
+        self.__dict__['_config'][name] = value
         
     def save(self):
         torch.save(self._config, self.config_file_path)
