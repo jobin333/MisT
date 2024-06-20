ee91d52d4d08a8e0dc9bcdd129d8b0c5d6279e70
jobin333 <jobin.jbn@gmail.com>
2024-06-20 20:37:38 +0530

    updated config_generator for overwriting config file

--- mvit/train_utils/config_generator.py
+++ mvit/train_utils/config_generator.py
@@ -10,6 +10,61 @@ from mvit.metrics.metrics import Accuracy
 
 
 class TrainerConfigurationGenerator():
+
+    '''
+    Parameters:-
+    __init__(dataset_name, feature_model_name, basic_config) 
+
+    dataset_details = {
+                        'cholec80':{
+                            'video_count':80, 
+                            'num_classes':7, 
+                            'test_file_count': 11,
+                        },
+        
+                        'm2cai16':{
+                            'video_count':[27, 14], 
+                            'num_classes':8, 
+                
+                        },
+        
+                        'autolaparo':{
+                            'video_count':21, 
+                            'num_classes':7, 
+                            'test_file_count': 3,
+                        
+                        }
+                    }
+
+    feature_model_details = { 'Swin3D_B':1024, 'Swin3D_S':768, 'Swin3D_T':768 }
+
+
+    configs = {
+        'dataset_details': dataset_details,
+        'feature_model_details': feature_model_details,
+        'feature_folder' : '/workspace/VideoImageFeatures',
+        'model_save_folder' : '/workspace/VideoImageModels',
+        'random_train_test' : False,
+        'overwrite_config': True,
+        
+        'flm_seq_length' : 30,
+        'flm_lr' : 0.0001,
+        'flm_max_epoch' : 100,
+        'flm_stop_epoch_count': 20,
+        'flm_model_name': 'Simple_Linear',
+        
+        'slm_lr' : 0.0001,
+        'slm_max_epoch' : 100,
+        'slm_stack_length' : 60,
+        'slm_roll_count' : 20,
+        'slm_number_path' : 6,
+        'slm_path_multiplier' : 2,
+        'slm_dropout' : 0.0,
+        'slm_stop_epoch_count': 7,
+        'slm_model_name': 'Multilevel_Linear',
+
+    }
+    '''
     
     def __init__(self, *args):
         if len(args) == 1: 
@@ -57,7 +112,7 @@ class TrainerConfigurationGenerator():
             config_file_name = f'{dataset_name}_{feature_model_name}_{idx}_pt.config'
             config_file_path = os.path.join(self.model_save_folder, config_file_name)
 
-            if not os.path.exists(config_file_path):
+            if not os.path.exists(config_file_path) or self.overwrite_config:
                 break
 
         return flm_save_param_path, slm_save_param_path, config_file_path        
