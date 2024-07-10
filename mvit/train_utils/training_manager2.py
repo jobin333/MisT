2b4256c370b3259b24159676a94b68d118f118d3
jobin333 <jobin.jbn@gmail.com>
2024-07-10 15:32:56 +0530

    error correction of training_manager2

--- mvit/train_utils/training_manager2.py
+++ mvit/train_utils/training_manager2.py
@@ -30,14 +30,14 @@ class TrainingManager():
     self.config_files = self.get_config_files(config_folder)
     self.device = device
     self.metrics = metrics
-    self.retrain = retrains
+    self.retrain = retrain
     self.flm_model_class = flm_model_class
     self.slm_model_class = slm_model_class
     self.cfg = TrainerConfigurationGenerator(self.config_file)
-    self.dataset_manager = ModelOuptutDatasetManager(cfg.feature_folder, cfg.feature_model_name, cfg.dataset_name,
-                                                cfg.train_file_indices, cfg.test_file_indices,  seq_length=cfg.flm_seq_length, 
-                                                device=self.device, in_test_set=cfg.contain_test_set)
-    self.flm = self.flm_model_class(in_features=cfg.in_features, out_features=cfg.out_features, seq_length=cfg.flm_seq_length)
+    self.dataset_manager = ModelOuptutDatasetManager(self.cfg.feature_folder, self.cfg.feature_model_name, self.cfg.dataset_name,
+                                                self.cfg.train_file_indices, self.cfg.test_file_indices,  seq_length=self.cfg.flm_seq_length, 
+                                                device=self.device, in_test_set=self.cfg.contain_test_set)
+    self.flm = self.flm_model_class(in_features=self.cfg.in_features, out_features=self.cfg.out_features, seq_length=self.cfg.flm_seq_length)
 
 
   def get_config_files(self, config_folder):
@@ -46,60 +46,58 @@ class TrainingManager():
     return config_files
 
   def train_flm(self):
-    cfg = self.cfg
-    if not self.retrain and cfg.flm_training_completed:
+    if not self.retrain and self.cfg.flm_training_completed:
       print('Training already completed exiting')
       return
     
     trainer = Trainer(self.dataset_manager, self.device, self.metrics, self.flm,
-                   save_model_param_path=cfg.flm_save_param_path,
+                   save_model_param_path=self.cfg.flm_save_param_path,
                    loss_fn=self.flm_loss_fn, optimizer_fn=torch.optim.Adam, 
-                   optimizer_params={'lr':cfg.flm_lr}, save_during_training=False,
-                   stop_epoch_count=cfg.flm_stop_epoch_count, model_name = cfg.flm_model_name)
+                   optimizer_params={'lr':self.cfg.flm_lr}, save_during_training=False,
+                   stop_epoch_count=self.cfg.flm_stop_epoch_count, model_name = self.cfg.flm_model_name)
     
-    trainer.train(cfg.flm_max_epoch)
+    trainer.train(self.cfg.flm_max_epoch)
     
     for metric in  self.metrics:
       if hasattr(metric, 'details'):
           config_name = 'flm_' + metric.name + '_details'
           config_value = metric.details()
-          cfg.__setattr__(config_name, config_value)
+          self.cfg.__setattr__(config_name, config_value)
       config_name = 'flm_' + metric.name 
       config_value = metric.value()
-      cfg.__setattr__(config_name, config_value)
-    cfg.__setattr__('flm_training_completed', True)
+      self.cfg.__setattr__(config_name, config_value)
+    self.cfg.__setattr__('flm_training_completed', True)
     trainer.save_model()
-    cfg.save()
+    self.cfg.save()
         
 
     
   def train_slm(self):
-    cfg = self.cfg
-    slm = self.slm_model_class(predictor_model=self.flm, stack_length=cfg.slm_stack_length,
-                                  dropout=cfg.slm_dropout, 
-                                num_surg_phase=cfg.out_features, rolls=cfg.slm_rolls)
+    slm = self.slm_model_class(predictor_model=self.flm, stack_length=self.cfg.slm_stack_length,
+                                  dropout=self.cfg.slm_dropout, 
+                                num_surg_phase=self.cfg.out_features, rolls=self.cfg.slm_rolls)
 
     trainer = Trainer(self.dataset_manager, self.device, self.metrics, slm,
-                  save_model_param_path=cfg.slm_save_param_path,
+                  save_model_param_path=self.cfg.slm_save_param_path,
                   loss_fn=self.slm_loss_fn, optimizer_fn=torch.optim.Adam, 
-                  optimizer_params={'lr':cfg.slm_lr}, save_during_training=False,
-                  stop_epoch_count=cfg.slm_stop_epoch_count, model_name = cfg.slm_model_name)
+                  optimizer_params={'lr':self.cfg.slm_lr}, save_during_training=False,
+                  stop_epoch_count=self.cfg.slm_stop_epoch_count, model_name = self.cfg.slm_model_name)
     
-    trainer.train(cfg.slm_max_epoch)
+    trainer.train(self.cfg.slm_max_epoch)
 
     
     for metric in  self.metrics:
         if hasattr(metric, 'details'):
           config_name = 'slm_' + metric.name + '_details'
           config_value = metric.details()
-          cfg.__setattr__(config_name, config_value)
+          self.cfg.__setattr__(config_name, config_value)
         config_name = 'slm_' + metric.name + '_value'
         config_value = metric.value()
-        cfg.__setattr__(config_name, config_value)
+        self.cfg.__setattr__(config_name, config_value)
     
-    cfg.__setattr__('slm_training_completed', True)
+    self.cfg.__setattr__('slm_training_completed', True)
     trainer.save_model()
-    cfg.save()
+    self.cfg.save()
 
     # Code to return last metric details
     return metric.details() if hasattr(metric, 'details') else None
