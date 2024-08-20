20332e40deb9c67d38fa6dceb741cd0bc78e5118
jobin333 <jobin.jbn@gmail.com>
2024-08-20 09:29:46 +0530

    added MultiLevelTransformerMemoryModel

--- mvit/models/transformer_models.py
+++ mvit/models/transformer_models.py
@@ -1,4 +1,5 @@
 import math
+import torch
 
 from torch import nn, Tensor
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
@@ -50,4 +51,95 @@ class TransformerModel(nn.Module):
 
         output = self.flatten(output)
         output = self.linear(output)
+        return output
+
+
+class MultiLevelTransformerMemoryModel(torch.nn.Module):
+    '''
+    Expecting batch first format (Batch, Sequence, Features)
+    Stack of F(z)
+    
+    '''
+    def __init__(self, stack_length, strides, dropout, nhead, dim_feedforward, 
+                 num_layers, num_surg_phase, dmodel, device=None): 
+        super().__init__()
+
+        self.dmodel=dmodel
+        self.stack_length = stack_length
+        self.strides = torch.tensor(strides)
+        self.strides = self.strides.to(torch.int)
+        self.number_path = len(self.strides)
+        self.seq_length = len(self.strides)
+        self.num_surg_phase = num_surg_phase
+
+        self.mhat_reducer = torch.nn.Linear(self.num_surg_phase*stack_length, self.dmodel)
+        self.output_projection = torch.nn.Linear(self.seq_length*self.dmodel, num_surg_phase)
+        
+        self.flatten = torch.nn.Flatten()
+        self.softmax = torch.nn.Softmax(-1)
+        self.relu = torch.nn.ReLU()
+        self.positional_encoding = self.generate_positional_encoding()
+        self.transformer_encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self.dmodel, 
+                                                                          nhead=nhead, batch_first=True,
+                                                             dim_feedforward=dim_feedforward )
+        self.transformer_encoder = torch.nn.TransformerEncoder(self.transformer_encoder_layer, 
+                                                               num_layers=num_layers)
+        
+        if device is not None:
+            self.device = device
+        else:
+            self.device = torch.device('cuda' if torch.cuda().is_available else 'cpu')
+        
+            
+
+    def generate_positional_encoding(self,):
+        position = torch.arange(self.seq_length).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, self.dmodel, 2) * (-math.log(10000.0) / self.dmodel))
+        pe = torch.zeros(self.seq_length, 1, self.dmodel) # max_len = seq_length
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe = pe.to(self.device)
+        return pe.transpose(1,0)
+        
+    
+    def disable_gradient(self, model):
+        for param in model.parameters():
+            param.requires_grad = False
+        
+    def reg_params(self, name, module):
+        for i, param in enumerate(module.parameters()):
+            self.register_parameter(name + str(i), param)
+    
+    def generate_stack(self, x, stack_length, strides):
+        if len(x.shape) == 3:  ## Already stacked
+            return x
+        stack_list = [x]
+        for i in range(stack_length-1):
+            shifted_x = torch.roll(stack_list[-1], strides, dims=0)
+            if strides > 0:
+                shifted_x[:strides] = 0.
+            else:
+                shifted_x[strides:] = 0.
+            stack_list.append(shifted_x)
+        stack = torch.stack(stack_list).permute(1,0,2)
+        return stack
+
+
+    def forward(self, x, attn_mask=None):
+        with torch.no_grad():
+            x = self.softmax(x)
+            xs = []
+            for i in self.strides:
+                xx = self.generate_stack(x, self.stack_length, i.item())
+                xs.append(xx) 
+        ys = []
+        for stride, x in zip(self.strides, xs):
+            y = self.flatten(x)
+            y = self.mhat_reducer(y)
+            ys.append(y)
+        output = torch.stack(ys).transpose(1,0)
+        output = self.relu(output)
+        output = self.transformer_encoder(output)
+        output = self.flatten(output)
+        output = self.output_projection(output)
         return output
