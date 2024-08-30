abaccfe63afc72f2797718e0b266a0e747087d6f
jobin333 <jobin.jbn@gmail.com>
2024-08-30 21:00:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='mvit',
     version='0.1',
-    description='Multiscale vision transformer for Cholec80 dataset classification',
+    description='Multi-stride History-aware Transformer for surgical phase classification',
     author='Jobin Jose',
     author_email='jobin.jbn@gmail.com',
     packages=find_packages(),
-    # install_requires=['av'],
+    install_requires=['av'],
 )
